6856845f2f201588a1c8b4682d8cf4f5b623196f
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-11 19:34:17 +0330

    test commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var a;
+var b = 2;
+var c = true;
+a = "Hello World";
+console.log(a, b, c);
---
+++ index.ts
@@ -0,0 +1,7 @@
+let a: string;
+let b: number = 2;
+let c: boolean = true;
+
+a = "Hello World";
+
+console.log(a, b, c);
