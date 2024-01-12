43c108a072b01a8b582340781ed7a5aab8c13ea3
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-13 02:02:33 +0330

    f

--- 09-fizz-buzz/fizz-buzz.js
+++ 09-fizz-buzz/fizz-buzz.js
@@ -1,3 +1,13 @@
-function fizzBuzz() {}
+function fizzBuzz(num) {
+  const arr = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("fizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("fizz");
+    }
+  }
+}
 
 module.exports = fizzBuzz;
