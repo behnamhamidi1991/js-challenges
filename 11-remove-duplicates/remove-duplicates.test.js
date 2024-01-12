f171d7df480348f26c7a940b13965ad45526706e
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-13 01:55:49 +0330

    Passed all tests

--- 11-remove-duplicates/remove-duplicates.test.js
+++ 11-remove-duplicates/remove-duplicates.test.js
@@ -1,16 +1,11 @@
 const removeDuplicates = require("./remove-duplicates");
 
 test("Removing duplicates from an array", () => {
-  expect(
-    removeDuplicates(["apple", "banana", "orange", "banana", "kiwi"]).toEqual([
-      "apple",
-      "banana",
-      "orange",
-      "kiwi",
-    ]);
-    expect(removeDuplicates([true, true, false, true, false])).toEqual([
-      true,
-      false,
-    ]);
-  );
+  expect(removeDuplicates([1, 2, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  expect(removeDuplicates(["apple", "banana", "banana", "orange"])).toEqual([
+    "apple",
+    "banana",
+    "orange",
+  ]);
+  expect(removeDuplicates([true, true, false, false])).toEqual([true, false]);
 });
