f16ff43d4d9234276ab0e7c2b7cd1532433a9d6e
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-13 01:56:44 +0330

    let's go for fizzBuzz

--- 09-fizz-buzz/fizz-buzz.js
+++ 09-fizz-buzz/fizz-buzz.js
@@ -1,13 +1,3 @@
-function fizzBuzz(i) {
-  for (i = 1; 1 <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      return "fizzBuzz";
-    } else if (i % 3 === 0) {
-      return "fizz";
-    } else if (i % 5 === 0) {
-      return "buzz";
-    }
-  }
-}
+function fizzBuzz() {}
 
 module.exports = fizzBuzz;
